b4d587bca2423d013c450ffc9bf266df3fdb5856
SU <recallsufuture@gmail.com>
2020-03-06 19:42:59 +0800

    feat: icon - 添加icon组件

---
+++ developing/icon/index.js
@@ -0,0 +1,15 @@
+// developing/icon/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: String,
+    size: null,
+    color: String,
+    prefix: {
+      type: String,
+      value: "joy-icon"
+    }
+  }
+})
