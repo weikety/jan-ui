480c4d7571cfdb602a12ffb8ccec1c96a7eebb93
Meeken <78071836@qq.com>
2020-03-08 12:53:58 +0800

    feat: cell-group - 添加圆角属性

--- developing/cell-group/index.js
+++ developing/cell-group/index.js
@@ -1,10 +1,16 @@
 /**
  * 组件：cell-group
- * 版本：v0.0.1
+ * 版本：v0.0.2
  * 维护人：Meeken
  */
 Component({
-  properties: {},
+  properties: {
+    customClass: String,
+    borderRadius: {
+      type: String,
+      value: ''
+    }
+  },
 
   data: {},
 
