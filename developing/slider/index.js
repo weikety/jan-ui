05d73bfcbcfa4ce169e2b775ec1ddbcab1013d79
SU <recallsufuture@gmail.com>
2020-03-12 17:44:51 +0800

    feat: slider - 添加 slider 组件

---
+++ developing/slider/index.js
@@ -0,0 +1,41 @@
+/** 
+ * 组件：slider
+ * 版本：v0.0.1
+ * 维护人：SU
+ */
+Component({
+  properties: {
+    value: {
+      type: Number,
+      value: 0
+    },
+
+    disabled: {
+      type: Boolean,
+      value: false
+    },
+
+    min: {
+      type: Number,
+      value: 0
+    },
+
+    max: {
+      type: Number,
+      value: 100
+    },
+
+    step: {
+      type: Number,
+      value: 1
+    }
+  },
+
+  data: {
+
+  },
+
+  methods: {
+
+  }
+})
