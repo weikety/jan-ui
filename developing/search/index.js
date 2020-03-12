264524f31edd6372d17561a8d68f65a0c20429f6
Meeken <78071836@qq.com>
2020-03-13 02:02:37 +0800

    feat: search - 搜素框

---
+++ developing/search/index.js
@@ -0,0 +1,86 @@
+/**
+ * 组件：search
+ * 版本：v0.0.1
+ * 维护人：Meeken
+ */
+const janComponent = require("../_common/jan-component")
+const mixinComponent = require("../_common/mixin-component")
+const dataHook = require("../_common/data-hook")
+
+/* 使用 janComponent 初始化组件配置 */
+
+let options = janComponent({
+  properties: {
+    label: String,
+    focus: Boolean,
+    error: Boolean,
+    disabled: Boolean,
+    readonly: Boolean,
+    inputAlign: String,
+    showAction: Boolean,
+    useActionSlot: Boolean,
+    useLeftIconSlot: Boolean,
+    useRightIconSlot: Boolean,
+    value: String,
+    leftIcon: {
+      type: String,
+      value: "search"
+    },
+    rightIcon: String,
+    placeholder: String,
+    placeholderStyle: String,
+    actionText: {
+      type: String,
+      value: "取消"
+    },
+    background: {
+      type: String,
+      value: "var(--wrapper-color, #fff)"
+    },
+    maxlength: {
+      type: Number,
+      value: -1
+    },
+    shape: {
+      type: String,
+      value: "square"
+    },
+    clearable: {
+      type: Boolean,
+      value: true
+    }
+  }
+})
+
+/* 监听 class 和 style 变化的方法 */
+
+const onPropsChange = function() {
+  const { value, background } = this.properties
+  this.setData({
+    _value: value || "",
+    _background: `background: ${background};`
+  })
+}
+
+options = mixinComponent(options, dataHook(["value"], onPropsChange))
+
+/* 初始化样式，将 onStyleChange 和 onPropsChange 添加到组件的 methods */
+
+options = mixinComponent(options, {
+  methods: {
+    onPropsChange
+  },
+
+  attached() {
+    // 在组件加载时执行样式的初始化
+    this.onPropsChange()
+  }
+})
+
+/* 混入其他配置项 */
+
+// options = mixinComponent(options, {
+//   /* 要混入的配置项 */
+// })
+
+Component(options)
