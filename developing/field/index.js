573a3ca0553ad3e9ed7662348150f31ddb288b50
Meeken <78071836@qq.com>
2020-03-13 02:28:20 +0800

    feat: field 中添加文字对齐方式

--- developing/field/index.js
+++ developing/field/index.js
@@ -1,6 +1,6 @@
 /**
  * 组件：field
- * 版本：v0.0.3
+ * 版本：v0.0.4
  * 维护人：Meeken
  */
 const janComponent = require("../_common/jan-component")
@@ -42,6 +42,10 @@ let options = janComponent({
       type: String,
       value: "text-align: left;color: red;"
     },
+    inputAlign: {
+      type: String,
+      value: "left"
+    },
     selectionEnd: {
       type: Number,
       value: -1
@@ -81,17 +85,21 @@ let options = janComponent({
 /* 监听 class 和 style 变化的方法 */
 
 const onPropsChange = function() {
-  const { value } = this.properties
+  const { value, inputAlign } = this.properties
   /**
    * _class 和 _style 是组件内部维护的，
    * 保存类名和样式的 data
    */
   this.setData({
-    _value: value
+    _value: value,
+    _inputAlign: `text-align: ${inputAlign};`
   })
 }
 
-options = mixinComponent(options, dataHook(["value"], onPropsChange))
+options = mixinComponent(
+  options,
+  dataHook(["value", "inputAlign"], onPropsChange)
+)
 
 /* 初始化样式，将 onStyleChange 和 onClassChange 添加到组件的 methods */
 
