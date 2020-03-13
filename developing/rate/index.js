fcc325d4f0a98fc7cd526e4f519632fdfedac0de
SU <recallsufuture@gmail.com>
2020-03-13 13:33:57 +0800

    fix: rate - 使用全局默认字体大小，删除调试信息

--- developing/rate/index.js
+++ developing/rate/index.js
@@ -4,8 +4,6 @@
  * 维护人：SU
  */
 const janComponent = require("../_common/jan-component")
-const mixinComponent = require("../_common/mixin-component")
-const dataHook = require("../_common/data-hook")
 
 /* 使用 janComponent 初始化组件配置 */
 
@@ -35,7 +33,7 @@ let options = janComponent({
 
     size: {
       type: String | Number,
-      value: "20px"
+      value: null
     },
 
     gutter: {
@@ -105,7 +103,6 @@ let options = janComponent({
 
   methods: {
     onSelect(event) {
-      console.log("onSelect", event);
       const { disabled, readonly } = this.properties;
       const { _value } = this.data;
       const { index } = event.currentTarget.dataset;
@@ -117,7 +114,6 @@ let options = janComponent({
     },
 
     onTouchMove(event) {
-      console.log("onTouchMove", event);
       const { touchable } = this.properties;
       if (!touchable) return;
 
@@ -155,62 +151,5 @@ let options = janComponent({
     }
   }
 })
-
-// /* 监听 class 和 style 变化的方法 */
-
-// const onClassChange = function() {
-//   const {
-//     /* 需要读取的属性 */
-//   } = this.properties
-//   /**
-//    * _class 和 _style 是组件内部维护的，
-//    * 保存类名和样式的 data
-//    */
-//   this.setData({
-//     _class: `新的类名`
-//   })
-// }
-
-// const onStyleChange = function() {
-//   const {
-//     /* 需要读取的属性 */
-//   } = this.properties
-//   this.setData({
-//     _style: `新的样式`
-//   })
-// }
-
-// /* 使用 dataHook 来监听 props 或 data 的变化 */
-
-// options = mixinComponent(
-//   options,
-//   dataHook(["这些属性变化，会触发 _style 刷新"], onStyleChange)
-// )
-
-// options = mixinComponent(
-//   options,
-//   dataHook(["这些属性变化，会触发 _class 刷新"], onClassChange)
-// )
-
-// /* 初始化样式，将 onStyleChange 和 onClassChange 添加到组件的 methods */
-
-// options = mixinComponent(options, {
-//   methods: {
-//     onClassChange,
-//     onStyleChange
-//   },
-
-//   attached() {
-//     // 在组件加载时执行样式的初始化
-//     this.onClassChange()
-//     this.onStyleChange()
-//   }
-// })
-
-/* 混入其他配置项 */
-
-// options = mixinComponent(options, {
-//   /* 要混入的配置项 */
-// })
 
 Component(options)
