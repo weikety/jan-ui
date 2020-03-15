963231dc676446b49f9e04413529418e2a2411fc
Meeken <78071836@qq.com>
2020-03-16 00:35:31 +0800

    feat: popup - v0.0.3

---
+++ src/popup/index.js
@@ -0,0 +1,47 @@
+/**
+ * 组件：popup
+ * 版本：v0.0.3
+ * 维护人：SU
+ */
+const janComponent = require("../_common/jan-component")
+
+/* 使用 janComponent 初始化组件配置 */
+
+let options = janComponent({
+  properties: {
+    show: {
+      type: Boolean,
+      value: false
+    },
+
+    "zindex": {
+      type: Number,
+      value: 100
+    },
+
+    duration: {
+      type: Number,
+      value: 400
+    },
+
+    position: {
+      type: String,
+      value: "center"
+    },
+
+    model: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  methods: {
+    onOverlayTap() {
+      if (!this.properties.model) {
+        this.triggerEvent("close")
+      }
+    }
+  }
+})
+
+Component(options)
